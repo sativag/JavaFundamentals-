d0e281ddc9bb574e91ce3e471906698a6e06f195
Sativa Garcia <garcsati@gmail.com>
2018-10-10 15:43:21 -0500

    My first method and comments

--- project/src/com/fundamentals/java/WelcomeToJava.java
+++ project/src/com/fundamentals/java/WelcomeToJava.java
@@ -1,10 +1,21 @@
 package com.fundamentals.java;
 
+/*
+ public - Access Modifier
+ class - File type
+ WelcomeToJava - Name of class
+ */
 public class WelcomeToJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome to Java"); 
+	someMethod();	
+			
 	}
 
+	// My first method
+	public static void someMethod( ) {
+		System.out.println("Welcome to Java"); 
+	}
+	
 }
