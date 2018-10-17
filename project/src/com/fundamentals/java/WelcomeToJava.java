8001db05d522a93da755935af464b6825e2a4bcf
Sativa Garcia <garcsati@gmail.com>
2018-10-17 15:40:51 -0500

    Operator Examples

--- project/src/com/fundamentals/java/WelcomeToJava.java
+++ project/src/com/fundamentals/java/WelcomeToJava.java
@@ -1,4 +1,5 @@
 package com.fundamentals.java;
+
 import java.util.Scanner;
 
 /*
@@ -10,18 +11,43 @@ public class WelcomeToJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	//someMethod();
-	//stringExamples();
-	//moreStringExamples();
-	// primitiveExamples();
-		scannerExample(); 
-			
+		// someMethod();
+		// stringExamples();
+		// moreStringExamples();
+		// primitiveExamples();
+		// scannerExample();
+		// myOperatorExample();
+		myAssignmentExample();
+
+	}
+	
+	public static void myAssignmentExample() {
+		AssignmentExample ae = new AssignmentExample();
+		ae.plusEqualsExample();
+		ae.minusEqualsExample();
+		ae.multiplyEqualsExample(); 
+		ae.divisionEqualsExample();
+		ae.modulusEqualsExample();
+		ae.leftShiftEqualsExample();
+		ae.rightShiftEqualsExample();
+		ae.bitwiseAndEqualsExample();
+		ae.bitwiseOrEqualsExample();
+	}
+	
+	public static void myOperatorExample() {
+		OperatorExamples oe = new OperatorExamples();
+		oe.incrementSample();
+		oe.decrementSample();
+		oe.equalsExample();
+		oe.equalsExample2();
+		oe.logicalExample();
 	}
 
 	// My first method
-	public static void someMethod( ) {
-		System.out.println("Welcome to Java"); 
+	public static void someMethod() {
+		System.out.println("Welcome to Java");
 	}
+
 	public static void stringExamples() {
 		// String = Type | str1 = variable name | Welcome = string literal
 		String str1 = "Welcome";
@@ -34,15 +60,16 @@ public class WelcomeToJava {
 		String name = str2.concat(" is cool");
 		System.out.println(name);
 		// equals method will check to see if an object equals the string variable
-		boolean isMyString = str1.equals(str2);  // is str2 = str1?
-		System.out.println(isMyString); 
+		boolean isMyString = str1.equals(str2); // is str2 = str1?
+		System.out.println(isMyString);
 		// toLowerCase method will make the variable with lower case letters
 		String myString = str1.toLowerCase();
 		System.out.println(myString);
 		// toUpperCase method will make the variable with upper case letters
 		String mySecondString = str2.toUpperCase();
-		System.out.println(mySecondString);		
+		System.out.println(mySecondString);
 	}
+
 	public static void moreStringExamples() {
 		String myString = "My Java String";
 		// are these equal?
@@ -58,7 +85,7 @@ public class WelcomeToJava {
 		boolean isContained = myString.contains("ava");
 		System.out.println(isContained);
 	}
-	
+
 	public static void primitiveExamples() {
 		PrimitiveExamples pe = new PrimitiveExamples();
 		pe.myFirstPrimitiveMethod();
@@ -69,8 +96,7 @@ public class WelcomeToJava {
 		pe.myModulus();
 		pe.myOrderOp();
 	}
-	
-	
+
 	// Example of the use of Scanner
 	public static void scannerExample() {
 		int x, y, z;
@@ -80,11 +106,7 @@ public class WelcomeToJava {
 		y = in.nextInt();
 		z = x + y;
 		System.out.println("Sum of entered integers = " + z);
-	
-		
-		
-		
-		
+        in.close();
 	}
-	
+
 }
